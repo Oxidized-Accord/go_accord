068eaa2bed6b9f883a2fc585b5149225d4aa9e96
Lyubomir <bvvn@protonmail.com>
2025-08-05 03:49:30 +0300

    Added db connetions and subdivided the code

--- messaging_service/main.go
+++ messaging_service/main.go
@@ -1,98 +1,30 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
-	"sync"
-	"time"
 
+	"messaging_service/db"
+	"messaging_service/functions"
 	pb "messaging_service/messaging/proto"
 
-	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
 )
 
-type server struct {
-	pb.UnimplementedMessagingServiceServer
-	messages      sync.Map
-	senderIndex   map[string][]string
-	receiverIndex map[string][]string
-	mu            sync.RWMutex
-}
-
-func (s *server) SendMessage(ctx context.Context, req *pb.SendRequest) (*pb.SendResponse, error) {
-	id := uuid.New().String()
-	timestamp := time.Now().UTC().Format(time.RFC3339)
-	msg := &pb.Message{
-		Id:        id,
-		Sender:    req.Sender,
-		Receiver:  req.Receiver,
-		Content:   req.Content,
-		Timestamp: timestamp,
-	}
-
-	s.messages.Store(id, msg)
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.senderIndex[req.Sender] = append(s.senderIndex[req.Sender], id)
-
-	s.receiverIndex[req.Receiver] = append(s.receiverIndex[req.Receiver], id)
-
-	return &pb.SendResponse{Message: msg}, nil
-
-}
-
-func (s *server) GetMessage(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
-	val, ok := s.messages.Load(req.Id)
-	if !ok {
-		return nil, status.Errorf(404, "message not found")
-	}
-	return &pb.GetResponse{Message: val.(*pb.Message)}, nil
-}
-
-func (s *server) ListMessagesBySender(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
-	s.mu.RLock()
-	ids := s.senderIndex[req.User]
-	s.mu.RUnlock()
-
-	var results []*pb.Message
-
-	for _, id := range ids {
-		if val, ok := s.messages.Load(id); ok {
-			results = append(results, val.(*pb.Message))
-		}
-	}
-	return &pb.ListResponse{Messages: results}, nil
-}
-
-func (s *server) ListMessagesByReceiver(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
-	s.mu.RLock()
-	ids := s.receiverIndex[req.User]
-	s.mu.RUnlock()
-
-	var results []*pb.Message
-
-	for _, id := range ids {
-		if val, ok := s.messages.Load(id); ok {
-			results = append(results, val.(*pb.Message))
-		}
-	}
-	return &pb.ListResponse{Messages: results}, nil
-}
-
 func main() {
+
+	database := db.Connect()
+	defer database.Close()
+
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterMessagingServiceServer(grpcServer, &server{
-		senderIndex:   make(map[string][]string),
-		receiverIndex: make(map[string][]string)})
+	srv := functions.NewMessagingServer(database)
+
+	pb.RegisterMessagingServiceServer(grpcServer, srv)
 	reflection.Register(grpcServer)
 
 	log.Println("gRPC server running on :50051")
